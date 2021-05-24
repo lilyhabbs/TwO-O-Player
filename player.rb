34634ae0074f6185e3fb83f460e09e796f3be4a6
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-24 19:30:17 +0000

    Add logic to update score and check if game is over

--- player.rb
+++ player.rb
@@ -1,8 +1,18 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :score
 
   def initialize name
-    @name
+    @name = name
     @score = 3
+  end
+
+  # reduce score if answer is incorrect
+  def decrement_score
+    @score -= 1
+  end
+
+  # check if player has no lives left
+  def game_over?
+    @score == 0
   end
 end
