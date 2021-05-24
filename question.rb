f545a1e738a7fe9b919c9b4331e02c6eb941a4f3
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-24 22:03:42 +0000

    Add logic to display question & verify answer

--- question.rb
+++ question.rb
@@ -5,4 +5,18 @@ class Question
     @num2 = rand(1..20)
     @sum = @num1 + @num2
   end
+
+  def display_question current_player
+    puts "#{current_player.name}: What does #{@num1} plus #{@num2} equal?"
+  end
+
+  # check answer
+  def check_answer current_player, player_guess
+    unless player_guess == @sum
+      puts "👎 SORRY! The correct answer is #{@sum}."
+      current_player.decrement_score
+    else
+      puts "👏 YES! You are correct."
+    end
+  end
 end
