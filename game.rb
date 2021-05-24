2da6b30dd6481255242ac50c2caa1b2d2feecc5f
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-24 22:07:09 +0000

    Add logic for each turn

--- game.rb
+++ game.rb
@@ -27,4 +27,22 @@ class Game
     puts "🎉 #{winner.name} wins with a score of #{winner.score}/3"
     puts "----- GAME OVER -----"
   end
+
+  def turn
+    # generate and display new question
+    question = Question.new
+    question.display_question @current_player
+
+    # get user guess and compare to correct answer
+    player_guess = gets.chomp.to_i
+    question.check_answer @current_player, player_guess
+    
+    # check if game should continue or end
+    if @current_player.game_over?
+      @current_player == @player1 ? winner = @player2 : winner = @player1
+      end_game winner
+    else
+      continue_game
+    end
+  end
 end
